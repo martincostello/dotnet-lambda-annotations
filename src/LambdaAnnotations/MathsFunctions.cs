6103a4b6b5dbdafa7ba9f04f98f6238f38eb3add
martincostello <martin@martincostello.com>
2022-01-19 14:50:21 +0000

    Add Amazon.Lambda.Annotations
    Add reference to Amazon.Lambda.Annotations and attempt to use it.

---
+++ src/LambdaAnnotations/MathsFunctions.cs
@@ -0,0 +1,24 @@
+﻿using Amazon.Lambda.Annotations;
+using Amazon.Lambda.Core;
+using HttpMethod = Amazon.Lambda.Annotations.HttpMethod;
+
+[assembly: LambdaSerializer(typeof(Amazon.Lambda.Serialization.SystemTextJson.DefaultLambdaJsonSerializer))]
+
+namespace LambdaAnnotations;
+
+public class MathsFunctions
+{
+    [LambdaFunction]
+    [RestApi(HttpMethod.Get, "/{x}/add/{y}")]
+    public int Add(int x, int y)
+    {
+        return x + y;
+    }
+
+    [LambdaFunction]
+    [RestApi(HttpMethod.Get, "/{x}/subtract/{y}")]
+    public int Subtract(int x, int y)
+    {
+        return x - y;
+    }
+}
---
+++ test/LambdaAnnotations.Tests/MathsFunctionsTest.cs
@@ -0,0 +1,32 @@
+﻿using Xunit;
+
+namespace LambdaAnnotations;
+
+public class MathsFunctionsTest
+{
+    [Fact]
+    public void Add_Adds_Numbers()
+    {
+        // Arrange
+        var function = new MathsFunctions();
+
+        // Act
+        var actual = function.Add(1, 2);
+
+        // Assert
+        Assert.Equal(3, actual);
+    }
+
+    [Fact]
+    public void Subtract_Subtracts_Numbers()
+    {
+        // Arrange
+        var function = new MathsFunctions();
+
+        // Act
+        var actual = function.Subtract(1, 2);
+
+        // Assert
+        Assert.Equal(-1, actual);
+    }
+}
