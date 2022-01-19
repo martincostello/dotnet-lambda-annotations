f19e6e6f7b418f3087e3278acc69661f05030749
martincostello <martin@martincostello.com>
2022-01-19 14:35:38 +0000

    Add skeleton solution
    Add basic skeleton solution from the Empty Lambda .NET SDK template.

---
+++ src/LambdaAnnotations/MyFunction.cs
@@ -0,0 +1,13 @@
+﻿using Amazon.Lambda.Core;
+
+[assembly: LambdaSerializer(typeof(Amazon.Lambda.Serialization.SystemTextJson.DefaultLambdaJsonSerializer))]
+
+namespace LambdaAnnotations;
+
+public class MyFunction
+{
+    public virtual string? ToUpper(string input)
+    {
+        return input?.ToUpperInvariant();
+    }
+}
---
+++ test/LambdaAnnotations.Tests/MyFunctionTest.cs
@@ -0,0 +1,19 @@
+﻿using Xunit;
+
+namespace LambdaAnnotations;
+
+public class MyFunctionTest
+{
+    [Fact]
+    public void TestToUpperFunction()
+    {
+        // Arrange
+        var function = new MyFunction();
+
+        // Act
+        var upperCase = function.ToUpper("hello world");
+
+        // Assert
+        Assert.Equal("HELLO WORLD", upperCase);
+    }
+}
